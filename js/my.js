d29db42c989ae50dd2ece5e8c2266d56e7d7974b
Michael Wilson <Michael Wilson>
2017-11-04 17:34:35 -0400

    smallNavbar

--- js/my.js
+++ js/my.js
@@ -4,7 +4,7 @@ $(window).scroll(function (event){
   var scroll = $(window).scrollTop();
   var temp = 0;
 
-if (scroll > 40){
+if (scroll > 100){
     console.log("We are scrolled: "+scroll);
     //document.getElementById("vanish").style.display = "none";
     $("#vanish").fadeOut(1500);
@@ -14,3 +14,15 @@ if (scroll > 40){
     $("#vanish").fadeIn(1000);
   }
 });
+
+function dropDown(){
+  console.log("working");
+  document.getElementById('bars').classList.toggle("change");
+  document.getElementById('myDropdown').classList.toggle("fontIncrease");
+  document.getElementById('web').classList.toggle("fontDecrease");
+}
+function attachHandlers(){
+  document.getElementById('bars').onclick = dropDown;
+}
+
+window.onload = attachHandlers;
